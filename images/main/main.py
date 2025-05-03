3971b4b763c7ba3eb97e295bc23637f1eca700b1
Mrinal Wadhwa <mrinal@ockam.io>
2025-05-03 12:14:14 -0700

    update template

--- images/main/main.py
+++ images/main/main.py
@@ -1,18 +1,10 @@
-from ockam import Agent, Model, Node
+from ockam import Agent, Node, Repl
 from sys import argv
 
 
 async def main(node):
-    agent = await Agent.start(
-        node=node,
-        model=Model("bedrock/anthropic.claude-3-5-sonnet-20241022-v2:0"),
-        instructions="""
-            You are Henry, an expert legal assistant.
-            You have in-depth knowledge of United States corporate law.
-        """,
-    )
-
-    await agent.repl(argv[1])
+    agent = await Agent.start(node, "You are Jack Sparrow.")
+    await Repl.start(agent, argv[1])
 
 
-Node.run(main)
+Node.start(main)
