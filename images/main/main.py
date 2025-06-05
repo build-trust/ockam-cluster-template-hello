89076b6d3d7987f75b8c526a92a648938bc09675
Mrinal Wadhwa <mrinal@ockam.io>
2025-06-05 06:12:44 -0700

    give the agent a name

--- images/main/main.py
+++ images/main/main.py
@@ -2,7 +2,7 @@ from ockam import Agent, Node
 
 
 async def main(node):
-    await Agent.start(node, "You are Jack Sparrow.")
+    await Agent.start(node, "You are Jack Sparrow.", "jack")
 
 
 Node.start(main)
