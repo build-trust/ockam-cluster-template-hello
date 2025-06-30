2897b60bc3d80d55860cd28c65d1ae19b7e78650
Mrinal Wadhwa <mrinal@ockam.io>
2025-06-29 21:42:48 -0700

    improve template

--- images/main/main.py
+++ images/main/main.py
@@ -2,7 +2,7 @@ from ockam import Agent, Node
 
 
 async def main(node):
-    agent = await Agent.start(node, "You are Jack Sparrow.", "jack")
+    await Agent.start(node, "You are Jack Sparrow", "jack")
 
 
 Node.start(main)
